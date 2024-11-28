ea30b6a28de00370c86d8a246b2af53d1804a417
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-11-28 13:48:11 +0900

    logout

--- web-front/src/components/Molecules/NavigationList.vue
+++ web-front/src/components/Molecules/NavigationList.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import { inject } from "vue";
+import { useRouter } from "vue-router";
+import { signOutInjectionKey } from "../../providers/AuthProviderInjectionKey";
 import NavigationLink from "../Atoms/NavigationLink.vue";
 import { NAVIGATION_PATH } from "../../constants/navigation";
 
+const router = useRouter();
+const signOut = inject(signOutInjectionKey);
+
 const top = NAVIGATION_PATH.TOP;
 const create = NAVIGATION_PATH.CREATE;
+
+const handleLogout = () => {
+  if (signOut) {
+    localStorage.removeItem("access_token");
+    signOut();
+    router.push(NAVIGATION_PATH.SIGNIN);
+  }
+};
 </script>
 
 <template>
@@ -11,6 +25,11 @@ const create = NAVIGATION_PATH.CREATE;
     <ul class="ul">
       <NavigationLink title="Top" :path="top" />
       <NavigationLink title="Create" :path="create" />
+      <li class="li">
+        <button class="button" type="button" @click="handleLogout">
+          SignOut
+        </button>
+      </li>
     </ul>
   </nav>
 </template>
@@ -21,5 +40,31 @@ const create = NAVIGATION_PATH.CREATE;
   padding-left: 0;
   display: flex;
   justify-content: space-around;
+}
+
+.li {
+  list-style-type: none;
+}
+
+.button {
+  font-family: "Times New Roman", Times, serif;
+  width: 100%;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  appearance: none;
+  transition: 0.3s;
+  display: block;
+  background-color: #fff;
+  text-decoration: none;
+  color: #008080;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 12px 20px;
+  border-radius: 10px;
+}
+
+.button:hover {
+  background-color: #d8d8d8;
 }
 </style>
